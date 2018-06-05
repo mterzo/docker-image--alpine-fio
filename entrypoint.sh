b9f27c22e115e9113af4fdb6dc0b3d7c2956118f
Mike Terzo <mike@terzo.org>
2018-06-05 19:44:44 -0400

    Move logout to results directory.
    Pass command line arguments to fio when running a job

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-exec > >(tee ${HOSTNAME}.log)
+exec > >(tee /fio/results/${HOSTNAME}.log)
 
 FIO=/usr/local/bin/fio
 JOB_DIR=/fio/jobs
@@ -8,7 +8,7 @@ JOB_DIR=/fio/jobs
 if [ ! -z "$(ls -A ${JOB_DIR}/*.fio)" ]; then
     for job in ${JOB_DIR}/*.fio
     do
-        ${FIO} ${job}
+        ${FIO} ${job} $@
     done
 else
     ${FIO} $@
