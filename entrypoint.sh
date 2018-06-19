e18ebda926be634939dd720cf555de6cb5a5b526
Mike Terzo <mike@terzo.org>
2018-06-19 01:19:49 -0400

    Update entrypoint.sh
    Ignore error output from ls.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ exec > >(tee /fio/results/output-${HOSTNAME}.log)
 FIO=/usr/local/bin/fio
 JOB_DIR=/fio/jobs
 
-if [ ! -z "$(ls -A ${JOB_DIR}/*.fio)" ]; then
+if [ ! -z "$(ls -A ${JOB_DIR}/*.fio 2> /dev/null)" ]; then
     for job in ${JOB_DIR}/*.fio
     do
         ${FIO} $@ ${job}
